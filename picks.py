d20bf76322c7a7b09a987ffc0fbf6478c6c11d9e
Kambiz Tavabi <ktavabi@uw.edu>
2018-06-07 16:20:33 -0700

    Updated processing script

---
+++ picks.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+"""Subject IDs
+    410_15a No resting state
+    103_9a highpass > DC
+    104_9a highpass > DC
+    303_13a highpass > DC
+    pilots
+         genz901
+         genz904
+         genz907_9a
+         genz923
+         genz941
+         genz970
+         genz974
+"""
+
+names = ['genz101_9a',
+         'genz105_9a',
+         'genz106_9a',
+         'genz204_11a',
+         'genz205_11a',
+         'genz208_11a',
+         'genz209_11a',
+         'genz211_11a',
+         'genz212_11a',
+         'genz302_13a',
+         'genz304_13a',
+         'genz305_13a',
+         'genz306_13a',
+         'genz307_13a',
+         'genz308_13a',
+         'genz310_13a',
+         'genz311_13a',
+         'genz313_13a',
+         'genz401_15a',
+         'genz402_15a',
+         'genz403_15a',
+         'genz406_15a',
+         'genz408_15a',
+         'genz501_17a',
+         'genz502_17a',
+         'genz503_17a',
+         'genz504_17a',
+         'genz505_17a',
+         'genz506_17a',
+         'genz508_17a',
+         'genz510_17a']
+
+bad_channels = [['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743', 'MEG2542'],
+                ['MEG1743', 'MEG2542'],
+                ['MEG1743', 'MEG2542'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743', 'MEG2542'],
+                ['MEG1743'],
+                ['MEG1743', 'MEG2542'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743', 'MEG2542'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743'],
+                ['MEG1743']]
+
+
+
--- process.py
+++ process.py
@@ -1,61 +1,65 @@
 # -*- coding: utf-8 -*-
 """
 GenZ pilot analysis script.
-@authors: larsoner & mdclarke
-
+@author: Kambiz Tavabi
+@contact: ktavabi@gmail.com
+@license: MIT
+@date: 04/21/2018
 """
-#Notes:
 
-#NOT COMPLETE
-# genz301
-
-#NO SSS
-# genz504 & genz510 (exit 6)
-
-#NO SCORE
-# genz203: 66
-# genz506: 48
-# genz923: 120
-# genz993: 120
-
-#RuntimeError: Event time samples were not unique
-# genz508
-
-#NO MRI
-# genz404
-
-# skip 2,3,8,11,12,13,15
-
+import os.path as op
 import mnefun
 import numpy as np
+from picks import names, bad_channels
 
 params = mnefun.Params(tmin=None, tmax=None, n_jobs=18,
                        decim=1, proj_sfreq=200,
                        n_jobs_fir='cuda', n_jobs_resample='cuda',
                        filter_length='auto', lp_cut=80., bmin=None,
                        lp_trans='auto', bem_type='5120')
-sub_ids = [103, 104, 201, 203, 302, 303, 305, 308, 404, 501, 502, 503,
-           504, 506, 508, 510]
-params.subjects = ['genz%d_ses1' %ss for ss in sub_ids]
-params.subject_indices = np.arange(len(params.subjects))
+
+
+params.subjects = names
+params.subject_indices = np.setdiff1d(np.arange(len(params.subjects)),
+                                      np.array([3]))
+# write prebads
+for si, subj in enumerate(params.subjects):
+    if op.exists(op.join(params.work_dir, subj, 'raw_fif')):
+        prebad_file = op.join(params.work_dir, subj,
+                              'raw_fif', '%s_prebad.txt' % subj)
+        if not op.exists(prebad_file):
+            assert len(bad_channels) == len(params.subjects)
+            if bad_channels[si] is None:
+                with open(prebad_file, 'w') as f:
+                    f.write("")
+            else:
+                with open(prebad_file, 'w') as output:
+                    for ch_name in bad_channels[si]:
+                        output.write("%s\n" % ch_name)
+
 params.dates = [None] * len(params.subjects)
-bids_affix = '-1_fsmempr_ti1200_rms_1_freesurf_hires'
 params.structurals = ['sub%s' % ss for ss in params.subjects]
 params.subject_run_indices = None
-params.subjects_dir = '/storage/Maggie/anat/subjects/'
+params.subjects_dir = '/brainstudio/data/genz/freesurf_subjs'
 params.score = None
-params.run_names = ['%s_resting_01']
-params.acq_ssh = 'kam@minea.ilabs.uw.edu'
+params.run_names = ['%s_rest_01']
+params.acq_ssh = 'kambiz@minea.ilabs.uw.edu'
 params.acq_dir = ['/sinuhe/data01/genz', '/sinuhe/data03/genz']
-params.sws_ssh = 'kambiz@kasga.ilabs.uw.edu'  # kasga
+params.sws_ssh = 'kam@kasga.ilabs.uw.edu'  # kasga
 params.sws_dir = '/data07/kam/genz'
 params.sss_type = 'python'
 params.sss_regularize = 'in'
 params.st_correlation = 0.98
 params.trans_to = 'twa'
 params.tsss_dur = 300.
-params.movecomp = 'inter'
-params.coil_t_window = 'auto'
+# Set the parameters for head position estimation:
+params.coil_dist_limit = 0.01
+params.coil_t_window = 'auto'  # use the smallest reasonable window size
+# remove segments with < 3 good coils for at least 1 sec
+params.coil_bad_count_duration_limit = 1.  # sec
+# Annotation params
+params.rotation_limit = 20.  # deg/s
+params.translation_limit = 0.01  # m/s
 # Trial rejection
 params.reject = dict()
 params.proj_ave = True
@@ -68,30 +72,24 @@ params.proj_nums = [[3, 3, 0],  # ECG: grad/mag/eeg
                     [3, 3, 0],  # EOG
                     [0, 0, 0]]  # Continuous (from ERM)
 params.on_missing = 'ignore'  # some subjects will not complete the paradigm
-# The ones we actually want
-params.analyses = []
-params.out_names = []
-params.out_numbers = []
-# do not trial count match for now
-params.must_match = []
 params.report_params.update(
     bem=True,
     psd=True,  # often slow
 )
 mnefun.do_processing(
     params,
-    fetch_raw=True,
+    fetch_raw=False,
     do_score=False,
     push_raw=False,
     do_sss=True,
     fetch_sss=False,
     do_ch_fix=False,
-    gen_ssp=True,
-    apply_ssp=True,
+    gen_ssp=False,
+    apply_ssp=False,
     write_epochs=False,
     gen_covs=False,
     gen_fwd=False,
     gen_inv=False,
-    gen_report=True,
+    gen_report=False,
     print_status=True,
 )
