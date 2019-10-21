eff47514e8f615498d110500ef852fa190b7a317
Kambiz Tavabi <ktavabi@gmail.com>
2019-10-21 14:21:53 -0700

    Script for sklearn clf & naive correlations

--- genz/processing/compute_graph_globals.py
+++ genz/processing/compute_graph_globals.py
@@ -34,8 +34,8 @@ def get_globals(G):
             nx.transitivity(G),
             nx.average_clustering(G),  # Global clustering coeff
             nx.density(G),  # resilience
-            nx.node_connectivity(G),  # minimum min-cut to disconnect
-            nx.average_node_connectivity(G),
+            # nx.node_connectivity(G),  # minimum min-cut to disconnect
+            # nx.average_node_connectivity(G),  # WAY TOO SLOW
             entropy(nx.eigenvector_centrality(G).values()))
 
 
@@ -45,8 +45,7 @@ picks.sort_values(by='id', inplace=True)
 dt = np.dtype('int, float')
 bands = list(defaults.bands.keys())
 metrics = ['char_len', 'diameter', 'transitivity',
-           'clustering', 'density',
-           'min_cut', 'connectivity', 'entropy']
+           'clustering', 'density', 'entropy']
 dims = ['band', 'subject', 'metric']
 
 for aix, age in enumerate(defaults.ages):
@@ -71,7 +70,8 @@ for aix, age in enumerate(defaults.ages):
                                       len(metrics)))
             metric_ds[ix, iix] = get_globals(G)
     # bands x subjects x metric
-    foo = xr.DataArray(metric_ds, coords=[['gamma'], [1], metrics], dims=dims)
+    foo = xr.DataArray(metric_ds, coords=[bands, subjects, metrics],
+                       dims=dims)
     foo.to_netcdf(op.join(defaults.datadir, 'genz_%s_globals.nc' % age))
     print('Run time %s ' % timestring(time.time() - t0))
     # bar = foo.to_dataframe().unstack('metric')
