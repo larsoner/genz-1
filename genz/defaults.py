0940afdd90e576ce856695a89140f240af29a5b0
Eric Larson <larson.eric.d@gmail.com>
2021-05-05 10:17:55 -0400

    ENH: Improve envcorr code (#2)
    * WIP: Improve envcorr code
    
    * FIX: Missed
    
    * ENH: Finished
    
    * FIX: Cleanup

--- genz/defaults.py
+++ genz/defaults.py
@@ -5,7 +5,7 @@
 
 """Global study parameters."""
 # TODO convert to YAML
- 
+
 from os import path as op
 from pathlib import Path
 
@@ -17,22 +17,15 @@ payload = op.join(Path(__file__).parents[0], "payload")
 archive = op.join(Path(__file__).parents[0], "archive")
 megdata = "/media/ktavabi/ALAYA/data/ilabs/genz_resting"
 subjects_dir = "/media/ktavabi/ALAYA/data/freesurfer"
-exclude = [
-    "104_9a",  # Too few EOG events
-    "108_9a",  # Fix
-    "113_9a",  # Too few ECG events
-    "115_9a",  # no cHPI
-    "121_9a",  # Source space missing points
-    "209_11a",  # Too few EOG events
-    "231_11a",  # twa_hp calc fail with assertion error
-    "427_15a",  # no epochs
-    "432_15a",  # Too few ECG events
-    "510_17a",  # Too few ECG events
-    "527_17a",  # Too few EOG events
-]  
+exclude = [  # these are unusable
+    "113_9a",  # unusable
+    "115_9a",  # missing resting recording
+    "318_13a",  # missing resting recording
+    "319_13a",  # unusable
+]
 # frequencies of interest
 bands = {
-    "DC": (0.01, 1.9),
+    "DC": (0, 1.9),
     "delta": (2, 4),
     "theta": (5, 7),
     "alpha": (8, 12),
@@ -44,15 +37,13 @@ bands = {
 ages = np.arange(9, 19, 2)
 
 # Down sample data to
-new_sfreq = 300.0
+new_sfreq = 250.0
 
 # Subjects with MEG data ACQ
 include = [
-    "104_9a",
     "105_9a",
     "106_9a",
     "108_9a",
-    "109_9a",
     "110_9a",
     "111_9a",
     "112_9a",
@@ -60,7 +51,6 @@ include = [
     "114_9a",
     "115_9a",
     "116_9a",
-    "117_9a",
     "118_9a",
     "119_9a",
     "120_9a",
@@ -71,30 +61,19 @@ include = [
     "125_9a",
     "126_9a",
     "128_9a",
-    "129_9a",
     "130_9a",
     "131_9a",
-    "132_9a",
     "133_9a",
-    "201_11a",
-    "202_11a",
-    "203_11a",
     "205_11a",
     "206_11a",
     "207_11a",
     "208_11a",
     "209_11a",
     "210_11a",
-    "211_11a",
     "212_11a",
-    "213_11a",
-    "214_11a",
     "216_11a",
     "218_11a",
     "219_11a",
-    "220_11a",
-    "221_11a",
-    "222_11a",
     "223_11a",
     "224_11a",
     "225_11a",
@@ -107,20 +86,12 @@ include = [
     "232_11a",
     "233_11a",
     "235_11a",
-    "302_13a",
-    "305_13a",
-    "307_13a",
-    "308_13a",
-    "309_13a",
-    "310_13a",
     "311_13a",
-    "312_13a",
     "313_13a",
     "314_13a",
-    "316_13a",
+    "318_13a",
     "319_13a",
     "320_13a",
-    "321_13a",
     "322_13a",
     "323_13a",
     "324_13a",
@@ -137,18 +108,14 @@ include = [
     "335_13a",
     "337_13a",
     "401_15a",
-    "402_15a",
     "403_15a",
-    "404_15a",
     "406_15a",
-    "408_15a",
-    "410_15a",
+    "409_15a",
     "411_15a",
     "412_15a",
     "413_15a",
     "414_15a",
     "415_15a",
-    "416_15a",
     "417_15a",
     "418_15a",
     "419_15a",
@@ -161,18 +128,11 @@ include = [
     "426_15a",
     "427_15a",
     "428_15a",
+    "429_15a",
+    "430_15a",
+    "431_15a",
     "432_15a",
-    "501_17a",
-    "502_17a",
-    "503_17a",
-    "504_17a",
-    "505_17a",
-    "506_17a",
     "507_17a",
-    "508_17a",
-    "509_17a",
-    "510_17a",
-    "511_17a",
     "512_17a",
     "513_17a",
     "514_17a",
@@ -185,9 +145,6 @@ include = [
     "521_17a",
     "522_17a",
     "523_17a",
-    "524_17a",
-    "525_17a",
-    "526_17a",
     "527_17a",
     "528_17a",
     "529_17a",
--- genz/processing/write_roi-aec_frames.py
+++ genz/processing/write_roi-aec_frames.py
@@ -6,29 +6,23 @@ For each subject, epoch raw .fif resting state into 5 sec. long arbitrary trials
 
 import os
 import os.path as op
-import warnings
+import time
 
-import matplotlib.pyplot as plt
-import mne
 import numpy as np
 import pandas as pd
-from autoreject import AutoReject
 from genz import defaults, funcs
-from meeg_preprocessing import config, utils
-from mne import compute_raw_covariance, read_epochs
+import mne
 from mne.connectivity import envelope_correlation
-from mne.cov import regularize
-from mne.filter import next_fast_len
-from mne.label import labels_to_stc
-from mne.minimum_norm import apply_inverse_epochs, make_inverse_operator
+from mne.minimum_norm import apply_inverse_epochs
 from mnefun import get_fsaverage_medial_vertices
 from scipy.sparse import csgraph
 
 ### locals
 new_sfreq = defaults.new_sfreq
-n_fft = next_fast_len(int(round(4 * new_sfreq)))
 lims = [75, 85, 95]
 medial_verts = get_fsaverage_medial_vertices()
+subjects_dir = defaults.subjects_dir
+n_jobs = 4
 
 # TODO refactor to YAML
 dfs = []
@@ -36,182 +30,148 @@ for ag in [9, 11, 13, 15, 17]:
     fi = op.join(defaults.static, "GenZ_subject_information - %da group.tsv" % ag)
     dfs.append(pd.read_csv(fi, sep="\t", usecols=["Subject Number", "Sex"]))
 df = pd.concat(dfs)
+df.reset_index(drop=True, inplace=True)
 df.columns = ["id", "sex"]
 df.sort_values(by="id")
 dups = df[df.duplicated("id")].index.values.tolist()
 df.drop(df.index[dups], inplace=True)
+df.id[:] = [id_.split('_', 1)[1] if isinstance(id_, str) else id_
+            for id_ in df.id]
 df.drop(df[df.id.isin(defaults.exclude)].index, inplace=True)
+df.drop(df[~df.id.isin(defaults.include)].index, inplace=True)
+assert len(df) == 106, len(df)
 df = df.dropna(how="any")
 # df = df.sample(5)
-rois = mne.read_labels_from_annot(
-    "fsaverage", "aparc_sub", "both", subjects_dir=defaults.subjects_dir
-)
-rois = [roi for roi in rois if not roi.name.startswith("unknown")]
+all_rois = mne.read_labels_from_annot(
+    "fsaverage", "aparc_sub", "both", subjects_dir=subjects_dir)
+rois = [roi for roi in all_rois if not roi.name.startswith("unknown")]
 roi_nms = [rr.name for rr in rois]
 n = len(roi_nms)
-data = np.zeros((len(df), len(defaults.bands), n))
+deg_lap = np.zeros((len(df), len(defaults.bands), n))
+degree = np.zeros_like(deg_lap)
 A_lst = list()
+reject = dict(grad=2000e-13, mag=6000e-15)  # same as GenZ repo
+src_fs = mne.read_source_spaces(
+    op.join(subjects_dir, "fsaverage", "bem", "fsaverage-ico-5-src.fif"))
 
 ###start subject loop
 for si, ss in enumerate(df.id.values):
-    subject = ss.replace("GenZ_", "genz")
-    print("    Loading data for %s" % subject)
-    bem_dir = os.path.join(defaults.subjects_dir, subject, "bem")
-    bem_fname = os.path.join(bem_dir, "%s-5120-bem-sol.fif" % subject)
-    src_fname = os.path.join(bem_dir, "%s-oct-6-src.fif" % subject)
+    subject = f'genz{ss}'
+    del ss
     subj_dir = os.path.join(defaults.megdata, subject)
+    inv_fname = os.path.join(
+        subj_dir, "inverse",
+        f"{subject}-meg-erm-inv.fif")
     raw_fname = os.path.join(
-        subj_dir, "sss_pca_fif", "%s_rest_01_allclean_fil100_raw_sss.fif" % subject
+        subj_dir, "sss_pca_fif",
+        f"{subject}_rest_01_allclean_fil80_raw_sss.fif"
     )
-    erm_fname = os.path.join(
-        subj_dir, "sss_pca_fif", "%s_erm_01_allclean_fil100_raw_sss.fif" % subject
-    )
-    trans_fname = os.path.join(subj_dir, "trans", "%s-trans.fif" % subject)
-    eps_dir = os.path.join(subj_dir, "epochs")
-    eps_fname = op.join(eps_dir, "All_sss_%s-epo.fif" % subject)
     src_dir = os.path.join(subj_dir, "source")
+    out_fname = os.path.join(src_dir, 'envcorr.h5')
+    if op.isfile(out_fname):
+        data = mne.externals.h5io.read_hdf5(out_fname)
+        degree[si] = data['degree']
+        deg_lap[si] = data['deg_lap']
+        continue
+    print("Loading data for %s" % subject)
     # Load raw
     try:
         raw = mne.io.read_raw_fif(raw_fname)
     except FileNotFoundError as e:
+        print(f'    File not found: {raw_fname}')
         continue
-    if not raw.info["highpass"] == 0:
-        print("%s acquisition HP greater than DC" % subject)
+    # 0.1 should be okay for 5 sec epochs (envcorr will baseline correct
+    # essentially because it's a correlation)
+    if raw.info["highpass"] > 0.11:
+        print(f"{subject} acquisition HP greater than 0.1 Hz "
+              f"({raw.info['highpass']})")
         continue
-    if not os.path.exists(eps_dir):
-        os.mkdir(eps_dir)
     if not os.path.exists(src_dir):
         os.mkdir(src_dir)
-    raw.load_data().resample(new_sfreq, n_jobs=config.N_JOBS)
-    raw_erm = mne.io.read_raw_fif(erm_fname)
-    raw_erm.load_data().resample(new_sfreq, n_jobs=config.N_JOBS)
-    raw_erm.add_proj(raw.info["projs"])
-    # ERM covariance
-    # TODO confirm covariance computed without rank warnings
-    cov = compute_raw_covariance(
-        raw_erm,
-        n_jobs=config.N_JOBS,
-        reject=dict(grad=4000e-13, mag=4e-12),
-        flat=dict(grad=1e-13, mag=1e-15),
-    )
-    # Make forward stack and get transformation matrix
-    src = mne.read_source_spaces(src_fname)
-    bem = mne.read_bem_solution(bem_fname)
-    trans = mne.read_trans(trans_fname)
-    fwd = mne.make_forward_solution(
-        raw.info, trans, src=src, bem=bem, eeg=False, verbose=True
-    )
+    raw.load_data()
     # epoch raw into 5 sec trials
-    print("      \nLoading ...%s" % op.relpath(eps_fname, defaults.megdata))
-    print(subject)
-    
+    print("    Loading epochs ...", end='')
+
     # start network loop
     a_lst = dict()
+    # epoch raw into 5 sec trials
+    events = mne.make_fixed_length_events(raw, duration=5.0)
+    tmax = 5.0 - 1. / defaults.new_sfreq
+    decim = 4
+    # first create originals to get drops
+    epochs = mne.Epochs(
+        raw, events=events, tmin=0, tmax=tmax, baseline=None,
+        reject=reject, preload=True, decim=4)
+    assert epochs.info['sfreq'] == defaults.new_sfreq
+    print(f" Dropped {len(events) - len(epochs)}/{len(events)} epochs")
+    events = epochs.events
     for ix, (kk, vv) in enumerate(defaults.bands.items()):
+        print(f'    Processing {"-".join(str(v) for v in vv)} Hz ...', end='')
+        t0 = time.time()
         hp, lp = vv
         a_lst[kk] = list()
-        # epoch raw into 5 sec trials
-        events = mne.make_fixed_length_events(raw, duration=5.0)
+        # now filter raw and re-epoch
+        hp = None if hp == 0 else hp
+        print(' Filtering ...', end='')
+        raw_use = raw.copy().filter(
+            hp, lp, l_trans_bandwidth=1, h_trans_bandwidth=1,
+            n_jobs=n_jobs)
         epochs = mne.Epochs(
-            raw,
-            events=events,
-            tmin=0,
-            tmax=5.0,
-            baseline=None,
-            reject=None,
-            preload=True,
-        )
-        if not op.isfile(eps_fname):
-            # k-fold CV thresholded artifact rejection
-            ar = AutoReject()
-            epochs = ar.fit_transform(epochs)
-            print("      \nSaving ...%s" % op.relpath(eps_fname, defaults.megdata))
-            epochs.save(eps_fname, overwrite=True)
-        epochs = read_epochs(eps_fname)
-        print(
-            "%d, %d (Epochs, drops)"
-            % (len(events), len(events) - len(epochs.selection))
-        )
-        # epochs.plot_psd()
-        roi_nms = np.setdiff1d(np.arange(len(events)), epochs.selection)
-        # raw = raw.copy().filter(lf, hf, fir_window='blackman',
-        #                       method='iir', n_jobs=config.N_JOBS)
-        iir_params = dict(order=4, ftype="butter", output="sos")
-        epochs_ = epochs.copy().filter(
-            hp, lp, method="iir", iir_params=iir_params, n_jobs=config.N_JOBS
-        )
-        # epochs_.plot_psd(average=True, spatial_colors=False)
-        mne.Info.normalize_proj(epochs_.info)
-        # epochs_.plot_projs_topomap()
-        
-        # regularize covariance
-        # rank = compute_rank(cov, rank='full', info=epochs_.info)
-        cov = regularize(cov, raw.info)
-        inv = make_inverse_operator(epochs_.info, fwd, cov)
-        
-        # Compute ROI time series and do envelope correlation
-        stcs = apply_inverse_epochs(
-            epochs_, inv, lambda2=1.0 / 9.0, pick_ori="normal", return_generator=True
-        )
-        label_ts = mne.extract_label_time_course(
-            stcs, rois, fwd["src"], return_generator=True, verbose=True
-        )
+            raw_use, events=epochs.events, tmin=0, tmax=tmax, baseline=None,
+            reject=None, preload=True, decim=decim)
+        epochs.apply_hilbert(envelope=False)
 
-        # compute ROI level envelop power
+        # Compute ROI time series and do envelope correlation
+        inv = mne.minimum_norm.read_inverse_operator(inv_fname)
+        these_rois = mne.morph_labels(
+            all_rois, subject, 'fsaverage', subjects_dir)
+        these_rois = [roi for roi in these_rois
+                      if not roi.name.startswith("unknown")]
+        stcs = apply_inverse_epochs(
+            epochs, inv, lambda2=1.0 / 9.0, pick_ori="normal",
+            return_generator=True)
+        label_ts = mne.extract_label_time_course(
+            stcs, these_rois, inv["src"], return_generator=True)
+
+        # compute ROI level envelope power
+        print(' Envcorr ...', end='')
         aec = envelope_correlation(label_ts)
         assert aec.shape == (len(rois), len(rois))
-        # compute ROI laplacian as per Ginset 
+        # compute ROI laplacian as per Ginset
         # TODO cite paper
-        _, deg_lap = csgraph.laplacian(aec, return_diag=True)
-            data[si, ix] = deg_lap 
-        
-        # compute ROI degree
-        degree = mne.connectivity.degree(aec, threshold_prop=0.2)
-        # if not np.allclose(deg_lap, degree):
-        #     warnings.warn("mne.connectivity.degree NOT equal to laplacian")
-        stc = mne.labels_to_stc(rois, degree)
-        stc = stc.in_label(
-            mne.Label(inv["src"][0]["vertno"], hemi="lh")
-            + mne.Label(inv["src"][1]["vertno"], hemi="rh")
-        )
-        morph = mne.compute_source_morph(
-            stc,
-            subject_from=None,
-            subject_to="fsaverage",
-            subjects_dir=defaults.subjects_dir,
-        )
-        a_lst[kk].append(morph.apply(stc))
-    A_lst.append(a_lst)
+        _, deg_lap[si, ix] = csgraph.laplacian(aec, return_diag=True)
 
-arrayostcs = np.array([[v[ii] for ii in defaults.bands] for v in A_lst]).squeeze()
-src = mne.read_source_spaces(
-    op.join(defaults.subjects_dir, "fsaverage", "bem", "fsaverage-oct6-src.fif")
-)
-vertices_to = [np.arange(10242)] * 2
+        # compute ROI degree
+        degree[si, ix] = mne.connectivity.degree(aec, threshold_prop=0.2)
+        # if not np.allclose(deg_lap, degree[si, ix]):
+        #     warnings.warn("mne.connectivity.degree NOT equal to laplacian")
+        print(f' Completed in {(time.time() - t0) / 60:0.1f} min')
+    mne.externals.h5io.write_hdf5(
+        out_fname, dict(degree=degree[si], deg_lap=deg_lap[si]))
+vertices_to = [s['vertno'] for s in src_fs]
 
 # visualize connectivity on fsaverage
 for ix, (kk, vv) in enumerate(defaults.bands.items()):
-    grab = arrayostcs[:, ix]
-    values = np.average([s.data for s in grab], axis=0)
-    this_stc = mne.SourceEstimate(values, vertices_to, tstep=0, tmin=0)
-    this_stc = this_stc.in_label(
-        mne.Label(src[0]["vertno"], hemi="lh") + mne.Label(src[1]["vertno"], hemi="rh")
-    )
+    grab = np.mean(degree[:, ix], axis=0)  # avg across subjects for this band
+    this_stc = mne.labels_to_stc(rois, grab, src=src_fs)
+    assert this_stc.data.shape == (20484, 1)
     brain = this_stc.plot(
         subject="fsaverage",
         clim=dict(kind="percent", lims=[75, 85, 95]),
         colormap="gnuplot",
-        subjects_dir=defaults.subjects_dir,
+        subjects_dir=subjects_dir,
         views="dorsal",
         hemi="both",
-        smoothing_steps=25,
+        time_viewer=False,
+        show_traces=False,
+        smoothing_steps='nearest',
         time_label="%s band" % kk,
     )
     brain.save_image(op.join(defaults.payload, "%s-nxDegree-group-roi.png" % kk))
 
 # write out NxROI laplacian to tidy CSV
 foo = funcs.expand_grid({"id": df.id.values, "freq": defaults.bands, "roi": roi_nms})
-foo["deg"] = pd.Series(data.flatten())
+foo["deg"] = pd.Series(deg_lap.flatten())
 foo.to_csv(op.join(defaults.payload, "nxLaplacian-roi.csv"))
 # bar = foo.pivot_table("deg", "id", ["freq", "roi"], aggfunc="first").to_csv(
 #     op.join(defaults.payload, "nxLaplnsXroi-wide.csv")
